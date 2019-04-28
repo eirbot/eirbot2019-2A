9e668cbb0edcc9ca82c525675cf05a4a43bcc52d
Mathieu PILLET <math.pillet@hotmail.com>
2019-04-28 16:27:06 +0200

    Add pid ports

--- hdl/lib/pid.v
+++ hdl/lib/pid.v
@@ -12,22 +12,26 @@
 module pid #(
 	parameter nbits = `PID_RES
 )(
-	// Global inputs
+	// Global ports
 	input clk,
 	input rst,
 	input clr,
 	input en,
-	// value inputs
-	input [nbits-1:0] u_i,
-	input [nbits-1:0] v_i,
-	// Output
-	output reg [nbits-1:0] out
+	// PID ports
+	input [nbits-1:0] sp_i,
+	input [nbits-1:0] pv_i,
+	output reg [nbits-1:0] co_o,
+	// ALU ports
+	input [nbits-1:0] alu_key_i,
+	input [7:0] alu_O_i,
+	output reg [7:0] alu_key_o,
+	output reg [7:0] alu_op_o,
+	output reg [nbits-1:0] alu_A_o,
+	output reg [nbits-1:0] alu_B_o
 );
 
 always @(posedge clk) begin
-	out <= u_i*v_i;
 end
-
 
 endmodule
 
