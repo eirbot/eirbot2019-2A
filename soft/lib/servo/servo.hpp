7b0a65def6787c2fdbfead14845f22bed9a37f7b
Mathieu PILLET <math.pillet@hotmail.com>
2019-05-26 16:31:06 +0200

    Change pos

--- soft/lib/servo/servo.hpp
+++ soft/lib/servo/servo.hpp
@@ -4,8 +4,9 @@
 
 #define SERVO_PWM_PRESCAL 59 // -1 pour avoir pile 50Hz sur l'oscillo, no lo sé why
 #define SERVO_PWM_PERIOD 60000 // 60 000 = 100% de pwm
-#define SERVO_DEPLOYED 1450 // Anciennement 0.025f avec PwmOUT
-#define SERVO_INIT 3870 // Anciennement 0.0645f avec PwmOUT
+#define SERVO_INIT 4250 // Anciennement 0.0645f avec PwmOUT
+#define SERVO_LEFT 1900 // Anciennement 0.025f avec PwmOUT
+#define SERVO_RIGHT 6550 // Anciennement 0.0645f avec PwmOUT
 
 #include <mbed.h>
 
